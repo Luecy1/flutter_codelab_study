694c437b82bc6f98b46ae6593b4c20db7a345368
Youhei Oonuma <oonuma617@gmail.com>
2020-11-24 22:14:23 +0900

    test

--- lib/test/main.dart
+++ lib/test/main.dart
@@ -1,7 +1,9 @@
 // https://codelabs.developers.google.com/codelabs/flutter-app-testing/
 
 import 'package:flutter/material.dart';
+import 'package:flutter_codelab_study/test/models/favorites.dart';
 import 'package:flutter_codelab_study/test/screens/home.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(TestingApp());
@@ -10,16 +12,19 @@ void main() {
 class TestingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Testing App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp(
+        title: 'Testing App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          HomePage.routeName: (context) => HomePage(),
+        },
+        initialRoute: HomePage.routeName,
       ),
-      routes: {
-        HomePage.routeName: (context) => HomePage(),
-      },
-      initialRoute: HomePage.routeName,
     );
   }
 }
--- lib/test/screens/home.dart
+++ lib/test/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codelab_study/test/models/favorites.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   static final String routeName = '/';
@@ -18,11 +20,35 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('Item $index'),
-        );
-      }),
+      body: ListView.builder(itemBuilder: (context, index) => ItemTile(itemNo: index)),
+    );
+  }
+}
+
+class ItemTile extends StatelessWidget {
+  final int itemNo;
+
+  const ItemTile({Key key, this.itemNo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var favoritesList = Provider.of<Favorites>(context);
+
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.primaries[itemNo % Colors.primaries.length],
+      ),
+      title: Text(
+        'Item $itemNo',
+        key: Key('text_$itemNo'),
+      ),
+      trailing: IconButton(
+        key: Key('icon_$itemNo'),
+        icon: favoritesList.items.contains(itemNo)
+            ? Icon(Icons.favorite)
+            : Icon(Icons.favorite_border),
+        onPressed: () {},
+      ),
     );
   }
 }
