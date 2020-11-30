7e29bac4d2b180fca20a4d0f4204ba8b51f92be0
Youhei Oonuma <oonuma617@gmail.com>
2020-11-30 21:59:23 +0900

    add test

--- test/test_app/home_test.dart
+++ test/test_app/home_test.dart
@@ -20,5 +20,25 @@ void main() {
       await tester.pumpAndSettle();
       expect(find.text('Item 0'), findsNothing);
     });
+
+    testWidgets('Test IconButtons', (tester) async {
+      // アプリの立ち上げ
+      await tester.pumpWidget(createHomeScreen());
+
+      // favorite iconがないこと
+      expect(find.byIcon(Icons.favorite), findsNothing);
+
+      await tester.tap(find.byIcon(Icons.favorite_border).first);
+      await tester.pumpAndSettle(Duration(seconds: 1));
+
+      expect(find.text('Added to favorites.'), findsOneWidget);
+      expect(find.byIcon(Icons.favorite), findsWidgets);
+
+      await tester.tap(find.byIcon(Icons.favorite).first);
+      await tester.pumpAndSettle(Duration(seconds: 1));
+
+      expect(find.text('Removed from favorites.'), findsOneWidget);
+      expect(find.byIcon(Icons.favorite), findsNothing);
+    });
   });
 }
