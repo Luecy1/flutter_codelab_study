452e8a438b0df85d46d99e3dab2ba4d47f3cc565
Youhei Oonuma <oonuma617@gmail.com>
2020-12-01 22:59:56 +0900

    error...

---
+++ lib/firebase_fcm/main.dart
@@ -0,0 +1,64 @@
+// respect https://note.com/welchi/n/n649728c5574d
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+main() async {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(),
+      body: MyPage(),
+    ),
+  ));
+}
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initNotification();
+  }
+}
+
+FirebaseMessaging _firebaseMessaging;
+
+Future<void> initNotification() async {
+  _firebaseMessaging = FirebaseMessaging()
+    ..requestNotificationPermissions()
+    ..onIosSettingsRegistered.listen((settings) {})
+    ..configure(
+      onMessage: (Map<String, dynamic> message) async {},
+      onBackgroundMessage: (Map<String, dynamic> message) async {
+        if (!Platform.isAndroid) {
+          return;
+        }
+
+        if (message.containsKey('data')) {
+          final data = message['data'];
+        }
+
+        if (message.containsKey('notification')) {
+          final notification = message['notification'];
+        }
+
+        print('onBackground :$message');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('onLaunch :$message');
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('onResume :$message');
+      },
+    );
+}
